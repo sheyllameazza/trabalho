09550aaf72a988e3b3ab8acaa4d662f55a54f2a8
sheyllameazza <101612694+sheyllameazza@users.noreply.github.com>
2022-05-30 17:57:35 -0400

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,13 @@
+$.support.cors = true;
+
+$('#btn-a').click(
+    function(){
+        $('main').load("./pages/pagina01.html")
+    }
+)
+
+$('#btn').click(
+    function(){
+        $('main').load("./pages/pagina02.html")  
+    }
+)
